51847a1467afa1365fa6879482bddd6dc51b7515
Miguel <faniksdo@gmail.com>
2023-04-25 20:25:03 -0500

    Primer commit

---
+++ js/logica_encriptador.js
@@ -0,0 +1,37 @@
+function encriptar(){
+    var texto= document.getElementById("texto-ingresado").value.toLowerCase();
+    var txtCifrado = texto.replace(/e/img,"enter");
+    var txtCifrado = txtCifrado.replace(/o/img,"over");
+    var txtCifrado = txtCifrado.replace(/i/img,"imes");
+    var txtCifrado = txtCifrado.replace(/a/img,"ai");
+    var txtCifrado = txtCifrado.replace(/u/img,"ufat");
+
+    document.getElementById("imagen_busqueda").style.display = "none";
+    document.getElementById("text_titulo").style.display = "none";
+    document.getElementById("text_descripcion").style.display = "none";
+    document.getElementById("texto-encriptado").innerHTML= txtCifrado;
+    document.getElementById("copear").style.display = "show";
+    document.getElementById("copear").style.display ="inherit";
+    
+}
+function desencriptar(){
+    var texto= document.getElementById("texto-ingresado").value.toLowerCase();
+    var txtCifrado = texto.replace(/enter/img,"e");
+    var txtCifrado = txtCifrado.replace(/over/img,"o");
+    var txtCifrado = txtCifrado.replace(/imes/img,"i");
+    var txtCifrado = txtCifrado.replace(/ai/img,"a");
+    var txtCifrado = txtCifrado.replace(/ufat/img,"u");
+
+    document.getElementById("imagen_busqueda").style.display = "none";
+    document.getElementById("text_titulo").style.display = "none";
+    document.getElementById("text_descripcion").style.display = "none";
+    document.getElementById("texto-encriptado").innerHTML = txtCifrado;
+    document.getElementById("copear").style.display = "show";
+    document.getElementById("copear").style.display ="inherit"; 
+}
+function copy(){
+    var contenito=document.querySelector("#texto-encriptado");
+    contenito.select();
+    document.execCommand("copy");
+    
+}

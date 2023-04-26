30db39d421accdea9147102e731b0bf272dd5c5a
Miguel <faniksdo@gmail.com>
2023-04-25 20:55:08 -0500

    arreglo de  encriptado o ober

--- js/logica_encriptador.js
+++ js/logica_encriptador.js
@@ -1,7 +1,7 @@
 function encriptar(){
     var texto= document.getElementById("texto-ingresado").value.toLowerCase();
     var txtCifrado = texto.replace(/e/img,"enter");
-    var txtCifrado = txtCifrado.replace(/o/img,"over");
+    var txtCifrado = txtCifrado.replace(/o/img,"ober");
     var txtCifrado = txtCifrado.replace(/i/img,"imes");
     var txtCifrado = txtCifrado.replace(/a/img,"ai");
     var txtCifrado = txtCifrado.replace(/u/img,"ufat");
@@ -17,7 +17,7 @@ function encriptar(){
 function desencriptar(){
     var texto= document.getElementById("texto-ingresado").value.toLowerCase();
     var txtCifrado = texto.replace(/enter/img,"e");
-    var txtCifrado = txtCifrado.replace(/over/img,"o");
+    var txtCifrado = txtCifrado.replace(/ober/img,"o");
     var txtCifrado = txtCifrado.replace(/imes/img,"i");
     var txtCifrado = txtCifrado.replace(/ai/img,"a");
     var txtCifrado = txtCifrado.replace(/ufat/img,"u");
